2584b281602e215e48a3daba5c3089812e745bd9
Dustin Kempton <dkempton.developer@gmail.com>
2018-07-31 07:42:27 -0700

    Pulled in bundled types from api def

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 const { makeExecutableSchema } = require('graphql-tools');
 const { parse } = require('api-parser');
 
+const { getTypes } = require('./types');
+
 const types = ``;
 
 const resolvers = {};
@@ -20,11 +22,13 @@ class Api {
     this.types = '';
     this.resolvers = {};
     this.schema = null;
+    this.api = {};
     this.init();
   }
   
   init() {
-    
+    this.api = parse(`${this.config.root_dir}/api.jaml`);
+    this.types = getTypes(this.config, this.api);
   }
 }
 
