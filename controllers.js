b70749a2de681c76fa1418625eeecc0c208e9580
Dustin Kempton <dkempton.developer@gmail.com>
2018-07-31 23:32:09 -0700

    Early stages of development. Basically just placeholder code

---
+++ controllers.js
@@ -0,0 +1,31 @@
+let controllers = null;
+
+const getControllers = (config, api) => {
+    return {
+        User: createController(),
+        Signing: createController(),
+        Address: createController()
+    }
+}
+
+/**
+ * 
+ * @param {Object} connector The connector that this object should use
+ */
+const createController = (connector) => {
+    return class {
+        constructor(obj, args, context, info) {
+            this.obj = obj;
+            this.args = args;
+            this.context = context;
+            this.info = info;
+        }
+    }
+}
+
+module.exports.getControllers = (config, api) => {
+    if(!controllers) {
+        controllers = getControllers(config, api);
+    }
+    return controllers;
+}
